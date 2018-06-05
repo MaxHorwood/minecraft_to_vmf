48c0b7a2776f9669043adc208f83b3b9772f6bbf
Max Horwood <Max-horwood@hotmail.com>
2018-06-05 12:10:39 +0100

    Work on the correct output for VMF brushes

--- js/brush.js
+++ js/brush.js
@@ -9,37 +9,47 @@ Brush = function(x, y, z, width, height, length) {
 
 // Converts the brush to a valid 'solid' for the VMF format https://developer.valvesoftware.com/wiki/VMF_documentation:_World_Class#solid
 // id needs to be unique
-Brush.prototype.toString = function(id) {
+Brush.prototype.getString = function(id) {
 
-   
-    var text =  "solid"+
-                "{"+
-                "\"id\" \""+id+"\""+
-                ""+ // all the sides ( 6 total )
-                "}"; 
+    var side = this.getSides(id);
+    
+    var text =  "solid\n"+
+                "{\n"+
+                "\t\"id\" \""+id+"\""+
+                "\n\t"+side[0]+
+                "\n\t"+side[1]+
+                "\n\t"+side[2]+
+                "\n\t"+side[3]+
+                "\n\t"+side[4]+
+                "\n\t"+side[5]+
+                "\n}";
+    return text;
 }
 
-Brush.prototype.getSides = function(x, y, z) {
+Brush.prototype.getSides = function(id) {
     var sides = [];
     // Loop all 6 sides of a box
-
+    for (var i = 0; i < 6; i++) {
+        sides.push(this.getSide((id += 1), "TOOLSNODRAW"));
+    }
 
     return sides;
 }
 
 // Will need to apply offset based on the side (top, bottom, left, right, front, back)
 // Will prob want to multiply the x, y, z to fit in the .VMF world (1 block = 32units)
-Brush.prototype.getSide = function(side, material_name) {
+Brush.prototype.getSide = function(id, material_name) {
     // This is for one side
     var id = "\"id\" \""+id+"\"";
-    var plane = "\"plane\" \"("+(this.x)+" "+(this.y)+" "+(this.z)+")\" \"("+(this.x)+" "+(this.y)+" "+(this.z)+")\" \"("+(this.x)+" "+(this.y)+" "+(this.z)+")\" ";
-    var mat = "\"material\" \"MINECRAFT/"+material_name+"\"";
+    var plane = "\"plane\" \"("+(this.x)+" "+(this.y)+" "+(this.z)+") ("+(this.x)+" "+(this.y)+" "+(this.z)+") ("+(this.x)+" "+(this.y)+" "+(this.z)+")\"";
+    // Will need own directory for custom materials in hammer...
+    var mat = "\"material\" \"TOOLS/"+material_name+"\"";
     var uaxis, vaxis;
     var rot = "\"rotation\" \"0\"";
     var lightmapscale = "\"lightmapscale\" \"16\"";
     var smooth = "\"smoothing_groups\" \"0\"";
     var disp = "dispinfo{}";
 
-    var comb = "side {" + id + "\n" + plane + "\n" + mat + "\n" + rot + "\n" + lightmapscale + "\n" + smooth + "\n" + disp + "\n }";
+    var comb = "side {\n\t\t" + id + "\n\t\t" + plane + "\n\t\t" + mat + "\n\t\t" + rot + "\n\t\t" + lightmapscale + "\n\t\t" + smooth + "\n\t\t" + disp + "\n\t}";
     return comb;
 }
