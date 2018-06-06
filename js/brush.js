3c51ace5aa0db6a53694f679eebabdf3c0891197
Max Horwood <Max-horwood@hotmail.com>
2018-06-06 23:57:40 +0100

    Actual text generation of VMF brush

--- js/brush.js
+++ js/brush.js
@@ -5,6 +5,12 @@ Brush = function(x, y, z, width, height, length) {
     this.w = width;
     this.h = height;
     this.l = length;
+    // Use nodraw as defualt.
+    this.material_name = "TOOLS/TOOLSNODRAW";
+}
+
+Brush.prototype.setMaterial = function(name) {
+    this.material_name = name;
 }
 
 // Converts the brush to a valid 'solid' for the VMF format https://developer.valvesoftware.com/wiki/VMF_documentation:_World_Class#solid
@@ -30,7 +36,7 @@ Brush.prototype.getSides = function(id) {
     var sides = [];
     // Loop all 6 sides of a box
     for (var i = 0; i < 6; i++) {
-        sides.push(this.getSide((id += 1), "TOOLSNODRAW"));
+        sides.push( this.getSide( (id += 1), i) );
     }
 
     return sides;
@@ -38,18 +44,87 @@ Brush.prototype.getSides = function(id) {
 
 // Will need to apply offset based on the side (top, bottom, left, right, front, back)
 // Will prob want to multiply the x, y, z to fit in the .VMF world (1 block = 32units)
-Brush.prototype.getSide = function(id, material_name) {
+Brush.prototype.getSide = function(id, side) {
     // This is for one side
-    var id = "\"id\" \""+id+"\"";
-    var plane = "\"plane\" \"("+(this.x)+" "+(this.y)+" "+(this.z)+") ("+(this.x)+" "+(this.y)+" "+(this.z)+") ("+(this.x)+" "+(this.y)+" "+(this.z)+")\"";
-    // Will need own directory for custom materials in hammer...
-    var mat = "\"material\" \"TOOLS/"+material_name+"\"";
+    var idt = "\"id\" \""+id+"\"";
+    var x1, x2, x3, z1, z2, z3, y1, y2, y3;
+    
+    var width = this.w * this.x;
+    var height = this.h * this.y;
+    var len = this.l * this.z;
+
+    y1 = height;
+    y2 = height;
+    y3 = height;
+    z1 = len;
+    z2 = len;
+    z3 = len;
+    x1 = width;
+    x2 = width;
+    x3 = width;
     var uaxis, vaxis;
+    switch(side) {
+        case 0: // top
+            y1 = height+this.h;
+            y2 = height+this.h;
+            y3 = height+this.h;
+            
+            z2 = len+this.l;
+            z3 = len+this.l;
+
+            x3 = width+this.w;
+            uaxis = "\"uaxis\" \"[1 0 0 0] 0.25\"";
+            vaxis = "\"vaxis\" \"[0 -1 0 0] 0.25\"";
+            break;
+        case 1: // bot
+            z1 = len+this.l;
+
+            x3 = width+this.w;
+            uaxis = "\"uaxis\" \"[1 0 0 0] 0.25\"";
+            vaxis = "\"vaxis\" \"[0 -1 0 0] 0.25\"";
+            break;
+        case 2:
+            y3 = height+this.h;
+            z2 = len+this.l;
+            z3 = len+this.l;
+            uaxis = "\"uaxis\" \"[0 1 0 0] 0.25\"";
+            vaxis = "\"vaxis\" \"[0 0 -1 0] 0.25\"";
+            break;
+        case 3: // Front
+            y3 = height+this.h;
+            x1 = width+this.w;
+            x2 = width+this.w;
+            x3 = width+this.w;
+            z1 = len+this.l;
+            uaxis = "\"uaxis\" \"[0 1 0 0] 0.25\"";
+            vaxis = "\"vaxis\" \"[0 0 -1 0] 0.25\"";
+            break;
+        case 4:
+            y3 = height+this.h;
+            x2 = width+this.w;
+            x3 = width+this.w;
+            z1 = len+this.l;
+            z2 = len+this.l;
+            z3 = len+this.l;
+            uaxis = "\"uaxis\" \"[1 0 0 0] 0.25\"";
+            vaxis = "\"vaxis\" \"[0 0 -1 0] 0.25\"";
+            break;
+        case 5:
+            y3 = height+this.h;
+            x1 = width+this.w;
+            uaxis = "\"uaxis\" \"[1 0 0 0] 0.25\"";
+            vaxis = "\"vaxis\" \"[0 0 -1 0] 0.25\"";
+            break;
+    }
+    var plane = "\"plane\" \"("+(x1)+" "+(z1)+" "+(y1)+") ("+(x2)+" "+(z2)+" "+(y2)+") ("+(x3)+" "+(z3)+" "+(y3)+")\"";
+    // Will need own directory for custom materials in hammer...
+    var mat = "\"material\" \""+this.material_name+"\"";
+    
     var rot = "\"rotation\" \"0\"";
     var lightmapscale = "\"lightmapscale\" \"16\"";
     var smooth = "\"smoothing_groups\" \"0\"";
-    var disp = "dispinfo{}";
+    //var disp = "dispinfo{}";
 
-    var comb = "side {\n\t\t" + id + "\n\t\t" + plane + "\n\t\t" + mat + "\n\t\t" + rot + "\n\t\t" + lightmapscale + "\n\t\t" + smooth + "\n\t\t" + disp + "\n\t}";
+    var comb = "side\n\t{\n\t\t" + idt + "\n\t\t" + plane + "\n\t\t" + mat + "\n\t\t" + uaxis + "\n\t\t"+ vaxis  + "\n\t\t" + rot + "\n\t\t" + lightmapscale + "\n\t\t" + smooth + "\n\t}";
     return comb;
 }
