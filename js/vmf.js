217861a3e393c4b1bb456f2cfdf5e18c236bbaf9
Max Horwood - Laptop <max-horwood@hotmail.com>
2018-07-04 22:35:46 +0200

    Additions to VMF creations

--- js/vmf.js
+++ js/vmf.js
@@ -10,10 +10,17 @@ VMF.prototype.addBlock = function(x, y, z, width, height, length) {
     this.world_brush_data.push(b);
 }
 // Creates the VMF file to be loaded in hammer
-VMF.prototype.createVmfWorld = function() {
-    // Loop through world brushes + 
+VMF.prototype.createVmfWorld = function(raw_block_data) {
+    // Loop through world brushes + add blocks
     var id = 0;
 
-    // Enter generic VMF world data here
+    for (var i = 0; i < raw_block_data.length; i++) {
+        this.addBlock(raw_block_data[i].x);
+    }
+
     // Now loop through brush, get string and make file.
+    for (var x = 0; x < this.world_brush_data.length; x++) {
+        var current_string = this.world_brush_data[x].getString(id);
+        // Append to file template.
+    }
 }
